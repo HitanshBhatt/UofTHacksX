abfaa5ed5c17d1d15c12d69a48ba766d77ad8980
Hitansh Bhatt <hitbhatt007@gmail.com>
2023-01-21 00:38:09 -0500

    fix changes

--- web_scrape.py
+++ web_scrape.py
@@ -11,13 +11,16 @@ from urllib.request import Request, urlopen
 #store extracted data into resource.txt file
 fileObject = open("resource.txt", "w")
 
+#fileObject.write(data)
+store_data = []
+
 for num in range(1,11):
     init_URL = 'https://mentalhealth.utoronto.ca/?sfid=205&sf_action=get_data&sf_data=results&sf_paged='+(str(num))+'&lang=en'
 
     req = Request(init_URL, headers = {'User-Agent': 'Mozilla/5.0'})
     res = urlopen(req).read()
     webpage = json.loads(res)["results"]
-    print(webpage)
+    #print(webpage)
 
     '''page = requests.get(init_URL)
     print(page.status_code) #print status code for debugging purposes
@@ -27,18 +30,22 @@ for num in range(1,11):
     soup = bs(webpage, "html.parser")
     #data = soup.get_text()
 
-    
-    #fileObject.write(data)
-    store_data = []
 
-    service_titles = soup.find_all('h3')
+    results = soup.find_all(attrs ={"class":"row service-item"})
 
     #print(service_titles)
-    for title in service_titles:
-        store_data.append((title.text))
+
+    #print(service_titles)
+    for result in results:
+        print(result)
+        title = result.find('h3')
+        location = result.find('div')
+        description = result.find('service-desc')
+        link = result.find('a')
+        store_data.append([title, location, description, link])
         #print(title.text, title.next_sibling)
 
-    for item in store_data:
-        fileObject.write(item + "\n")
+#fileObject.write(json.dumps(store_data, indent=4))
+print(store_data)
 
 
