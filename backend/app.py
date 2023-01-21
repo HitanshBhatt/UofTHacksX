21c4a21d5721d81e419af47be5c06cae38e09ad2
Jason Zhang <87998934+jasonz130@users.noreply.github.com>
2023-01-21 15:16:15 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,28 @@
-from flask import Flask
+# import sys
+# print (sys.path)
+
+from flask import Flask, render_template, request, jsonify
+# from chat import get_response
+
+
 app = Flask(__name__)
-@app.route('/')
-def back_end():
-    return "backend server test"
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    # text = request.get_json().get("message")
+
+    # response = get_response(text)
+    response = "hey there"
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
