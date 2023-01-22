1c30349b6a2a988382cea50d6c5f4e8d6a63d859
YahyaElgabra <elgabrayahya@gmail.com>
2023-01-21 23:37:09 -0500

    added cookies

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, request, make_response
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def back_end():
     return "backend server test"
+
+
+@app.route("/cookies")
+def cookies():
+    res = make_response("Cookies", 200)
+    res.set_cookie("rss_only", "True")
+    res.set_cookie("address", "")
+
+    return res
